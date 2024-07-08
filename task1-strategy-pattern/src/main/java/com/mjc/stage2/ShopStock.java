3a948d54a42f23c6cb5bb051ca97a69b5c6f706c
Mirabbos <mirabbosahmedov7777@gmail.com>
2024-07-08 14:36:58 +0500

    1 task is done

--- task1-strategy-pattern/src/main/java/com/mjc/stage2/ShopStock.java
+++ task1-strategy-pattern/src/main/java/com/mjc/stage2/ShopStock.java
@@ -1,13 +1,27 @@
 package com.mjc.stage2;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class ShopStock {
+public class ShopStock implements FilteringStrategy {
     private final List<Product> productList;
 
     public ShopStock(List<Product> productList) {
         this.productList = productList;
     }
 
-    // Write your code here!
+    @Override
+    public boolean filter(Product product) {
+        return false;
+    }
+
+    public List<Product> executeFilteringStrategy(FilteringStrategy filteringStrategy) {
+        List<Product> filteredProducts = new ArrayList<>();
+        for (Product product : productList) {
+            if (filteringStrategy.filter(product)) {
+                filteredProducts.add(product);
+            }
+        }
+        return filteredProducts;
+    }
 }
