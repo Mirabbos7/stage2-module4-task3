4efbeda80c84807d0004d245bf9da4ea96711210
Mirabbos <mirabbosahmedov7777@gmail.com>
2024-07-11 09:35:43 +0500

    Observer completed

--- task2-observer-pattern/src/main/java/com/mjc/stage2/impl/RectangleObserver.java
+++ task2-observer-pattern/src/main/java/com/mjc/stage2/impl/RectangleObserver.java
@@ -1,5 +1,41 @@
 package com.mjc.stage2.impl;
 
-public class RectangleObserver  {
-    // Write your code here!
+import com.mjc.stage2.Observer;
+import com.mjc.stage2.entity.Rectangle;
+import com.mjc.stage2.entity.RectangleValues;
+import com.mjc.stage2.event.RectangleEvent;
+import com.mjc.stage2.repository.RectangleRepository;
+import com.mjc.stage2.warehouse.RectangleWarehouse;
+
+public class RectangleObserver implements Observer {
+
+    @Override
+    public void handleEvent(RectangleEvent event) {
+        Rectangle rectangle = event.getSource();
+        double sideA = rectangle.getSideA();
+        double sideB = rectangle.getSideB();
+        double square = sideA * sideB;
+        double perimeter = 2 * (sideA + sideB);
+
+        RectangleValues rectangleValues = new RectangleValues(square, perimeter);
+        RectangleWarehouse.getInstance().put(rectangle.getId(), rectangleValues);
+    }
+
+    public static void main(String[] args) {
+        RectangleRepository repository = RectangleRepository.getInstance();
+        RectangleWarehouse warehouse = RectangleWarehouse.getInstance();
+
+        Rectangle rectangle = new Rectangle(1, 2.0, 3.0);
+        RectangleObserver observer = new RectangleObserver();
+
+        rectangle.addObserver(observer);
+        repository.add(rectangle);
+
+        rectangle.setSideA(4.0); // This should trigger the observer and update the warehouse
+        rectangle.setSideB(5.0); // This should also trigger the observer and update the warehouse
+
+        RectangleValues values = warehouse.get(rectangle.getId());
+        System.out.println("Square: " + values.getSquare());
+        System.out.println("Perimeter: " + values.getPerimeter());
+    }
 }
